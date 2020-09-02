c553a83c184bc4e86136299d4349513e2a17d338
st taro <stephentmccabe@gmail.com>
2020-09-02 08:53:12 -0700

    creating the geo distribution

---
+++ src/components/visualizer/GeometricDistribution.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function GeometricDistribution() {
+    return(
+        <div className="geometricDistribution">
+            <h1>geo distribution</h1>
+        </div>
+    );
+}
+
+export default GeometricDistribution
